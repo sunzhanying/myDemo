05ecd814187c3735e0998fbab60e11228445cc49
sunzy <32124308+sunzhanying@users.noreply.github.com>
2020-08-21 15:16:52 +0800

    filter

---
+++ ideaDemo/src/main/java/com/sunzy/demo/common/Constants.java
@@ -0,0 +1,10 @@
+package com.sunzy.demo.common;
+
+/**
+ * @author sunzy
+ * @date 2020/8/19
+ */
+public class Constants {
+    public static final String SESSION_SMS_USER_MOBILE = "15093104561";
+    public static final String SESSION_STAFF_INFO = "CST1022";
+}
---
+++ ideaDemo/src/main/java/com/sunzy/demo/common/OutputObject.java
@@ -0,0 +1,11 @@
+package com.sunzy.demo.common;
+
+/**
+ * @author sunzy
+ * @date 2020/8/19
+ */
+public class OutputObject {
+    public OutputObject(String code,String msg) {
+
+    }
+}
