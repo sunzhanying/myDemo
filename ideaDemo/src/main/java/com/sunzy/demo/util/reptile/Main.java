cb53655120562c8782f52d3d82a5ec294cc46682
sunzy <32124308+sunzhanying@users.noreply.github.com>
2020-09-30 17:57:06 +0800

    爬虫

--- ideaDemo/src/main/java/com/sunzy/demo/util/reptile/Main.java
+++ ideaDemo/src/main/java/com/sunzy/demo/util/reptile/Main.java
@@ -5,29 +5,102 @@ package com.sunzy.demo.util.reptile;
  * @date 2020/9/30
  */
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static  void  main(String [] args) {
 
-        for (int start = 0; start < 2; start ++)  {
+        for (int start = 0; start < 1; start ++)  {
             try {
                 //String address = "https://Movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&start=" + start;
                 String address = "http://oa.senshishui.com:20000/default/order/customerlist/page/1";
                 String currentStr = new GetJson().getHttpJson(address);
-                System.out.println("currentStr:" + currentStr);
-
+                //System.out.println("currentStr:" + currentStr);
+                getDocFromStr(currentStr);
                 //如果是json格式
                 //JSONObject jsonObject = JSON.parseObject(currentStr);
-                 System.out.println("已经爬取到底了");
-                //System.out.println("正在爬取中---共抓取:" + total + "条数据");
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
     }
 
+    private static String getDocFromStr(String str) throws Exception{
+        Document document = Jsoup.parse(str);
+        //解析HTML文件
+        Elements td = document.getElementsByTag("td");
+        int size = td.size();
+
+        List<String> listStr = new ArrayList<>();
+        for(int i = 0; i < size; i++){
+            if(i < 6 || i == 96){//去掉前六个和最后一个
+                continue;
+            }
+            Element element = td.get(i);
+            String innerStr = element.html();
+            listStr.add(innerStr);
+            System.out.println(innerStr);
+        }
+        List<String> listStr2 = new ArrayList<>();
+        /*for(int m = 0; m < listStr.size(); m++ ){
+            if(m % 6 == 0){
+                continue;
+            }
+            String ss2 = listStr.get(m);
+            listStr2.add(ss2);
+        }*/
+
+        List<List<String>> lists = averageAssign(listStr,15);
+
+        List<SenInfo> list = new ArrayList<>();
+        for(int i = 0; i < 15; i++){
+            SenInfo senInfo = new SenInfo();
+            List<String> list1 = lists.get(i);
+            senInfo.setBeginDate(list1.get(0));
+            senInfo.setCode(list1.get(1));
+            senInfo.setName(list1.get(2));
+            senInfo.setPhone(list1.get(3));
+            senInfo.setAddress(list1.get(4));
+            //senInfo.setHref(list1.get(5));
+            list.add(senInfo);
+        }
+        System.out.println(listStr);
+        System.out.println(listStr2);
+        System.out.println(lists);
+        System.out.println(list);
+        return "";
+    }
+
+    /**
+     * 将一组数据平均分成n组
+     *
+     * @param source 要分组的数据源
+     * @param n      平均分成n组
+     * @param <T>
+     * @return
+     */
+    public static <T> List<List<T>> averageAssign(List<T> source, int n) {
+        List<List<T>> result = new ArrayList<List<T>>();
+        int remainder = source.size() % n;  //(先计算出余数)
+        int number = source.size() / n;  //然后是商
+        int offset = 0;//偏移量
+        for (int i = 0; i < n; i++) {
+            List<T> value = null;
+            if (remainder > 0) {
+                value = source.subList(i * number + offset, (i + 1) * number + offset + 1);
+                remainder--;
+                offset++;
+            } else {
+                value = source.subList(i * number + offset, (i + 1) * number + offset);
+            }
+            result.add(value);
+        }
+        return result;
+    }
 }
---
+++ ideaDemo/src/main/java/com/sunzy/demo/util/reptile/SenInfo.java
@@ -0,0 +1,75 @@
+package com.sunzy.demo.util.reptile;
+
+import java.io.Serializable;
+
+/**
+ * @author sunzy
+ * @date 2020/9/30
+ */
+public class SenInfo implements Serializable {
+    private String beginDate;
+    private String code;
+    private String name;
+    private String phone;
+    private String address;
+    private String href;
+
+    public String getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(String beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    @Override
+    public String toString() {
+        return "SenInfo{" +
+                "beginDate='" + beginDate + '\'' +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
